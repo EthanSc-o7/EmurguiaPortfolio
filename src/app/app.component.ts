da93fb0a6d36e875ca96d173e342a458ca73936c
Ethan <113479501+EthanSc-o7@users.noreply.github.com>
2025-09-25 20:18:35 -0700

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,5 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   title = 'myPortfolio';
-  constructor(private router: Router) {
-    let path = localStorage.getItem('path');
-    if(path) {
-      localStorage.removeItem('path');
-      this.router.navigate([path]);
-    }
-  }
+
 }
